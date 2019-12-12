5bbb18fa724e8860d3fb4a496269ee86e651c120
Varun Nirala <nirala.varun@gmail.com>
2019-12-12 11:17:46 +0530

    chatApp : Updating with AndroidX

--- chat_app/ios/Flutter/flutter_export_environment.sh
+++ chat_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Program Files\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Varun\Varun_Work\FlutterProjects\Flutter\trunk\chat_app"
+export "FLUTTER_APPLICATION_PATH=D:\Varun\Varun_Work\FlutterProjects\chat_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
