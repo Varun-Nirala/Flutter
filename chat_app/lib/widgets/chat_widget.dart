fa286347cc9e85b0e86278dea3e3b4b8bd08ce8e
Varun Nirala <nirala.varun@gmail.com>
2019-12-13 15:55:25 +0530

    chatApp : Fixed compilation error of text composer

--- chat_app/lib/widgets/chat_widget.dart
+++ chat_app/lib/widgets/chat_widget.dart
@@ -15,32 +15,60 @@ class ChatWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Chat chat = Provider.of<ChatProvider>(context).getChat(chatId);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Flexible(
+            child: Container(
+              width: double.infinity,
+              color: Colors.red,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Text'),
+                ],
+              ),
+            ),
+          ),
+          Divider(height: 1),
+          textComposer(),
+        ],
+      ),
+    );
+  }
+
+  Row textComposer() {
+    return Row(
       children: <Widget>[
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Row(
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Type a message',
+                border: OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(50.0),
+                  ),
+                ),
+                prefixIcon: IconButton(
+                  icon: const Icon(Icons.insert_emoticon),
+                  onPressed: () {},
+                ),
+                suffixIcon: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          borderRadius: BorderRadius.circular(50)),
-                      child: TextFormField(
-                        initialValue: 'Type a message',
-                        showCursor: true,
-                      ),
-                    ),
+                    IconButton(
+                        icon: const Icon(Icons.attach_file), onPressed: () {}),
+                    IconButton(
+                        icon: const Icon(Icons.camera_alt), onPressed: () {}),
                   ],
                 ),
               ),
             ),
-            getCircularButton(Icon(Icons.mic), onMicPress),
-          ],
+          ),
         ),
+        getCircularButton(Icon(Icons.mic), onMicPress),
       ],
     );
   }
